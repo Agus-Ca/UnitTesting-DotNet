1e51d87e24e0961e4bd18d48a6459a29eec0d3d5
Agustín Casado <agu.e.casado@gmail.com>
2022-08-28 21:56:45 -0300

    unit test de un metodo que suma

---
+++ UnitTestingApp.UnitTests/Services/CalculadoraServiceTests/SumarTests.cs
@@ -0,0 +1,23 @@
+﻿using UnitTestingApp.Services;
+
+namespace UnitTestingApp.UnitTests.Services.CalculadoraServiceTests;
+
+public class SumarTests
+{
+    [Fact]
+    public void When_NumbersAreValid_Expect_TheirSum()
+    {
+        // Arrange
+        int number1 = 4;
+        int number2 = 5;
+        int expectedSum = 9;
+
+        CalculadoraService service = new();
+
+        // Act
+        var result = service.Sumar(number1, number2);
+
+        // Assert
+        Assert.Equal(expectedSum, result);
+    }
+}
---
+++ UnitTestingApp/Services/CalculadoraService.cs
@@ -0,0 +1,7 @@
+﻿namespace UnitTestingApp.Services;
+
+public class CalculadoraService
+{
+    public int Sumar(int numero1, int numero2)
+        => numero1 + numero2;
+}
